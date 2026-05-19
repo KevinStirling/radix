8f81809d6410959fc3b5070b2d4873673b10a0f8
Kevin Young <youngks93@gmail.com>
2026-05-18 21:45:17 -0400

    feature: add projectile weapon type

--- components/weapon/scripts/weapon_controller.gd
+++ components/weapon/scripts/weapon_controller.gd
@@ -1,6 +1,7 @@
 class_name WeaponController
 extends Node
 
+@export var camera: Camera3D
 @export var current_weapon: Weapon
 @export var weapon_model_parent: Node3D
 @export var weapon_state_chart: StateChart
@@ -33,3 +34,68 @@ func fire_weapon() -> void:
 	if can_fire():
 		current_ammo -= 1
 		print("Fired! Ammo: ", current_ammo)
+
+	if current_weapon.is_hitscan:
+		_perform_hitscan()
+	else:
+		_spawn_projectile()
+
+
+func _perform_hitscan() -> void:
+	if not camera:
+		print("no camera assigned")
+		return
+
+	var space_state = camera.get_world_3d().direct_space_state
+	var from = camera.global_position
+	var forward = -camera.global_transform.basis.z
+	var to = from + forward * current_weapon.max_range
+
+	var query = PhysicsRayQueryParameters3D.create(from, to)
+	# you can change collision mask here to restrict what mesh layers are hit
+	# query.collision_mask = 1
+	var result = space_state.intersect_ray(query)
+
+	if result:
+		print("Hit: ", result.collider.name, " at ", result.position)
+		_spawn_impact_marker(result.position)
+
+
+## Debug function to visualize where a hitscan weapon hits
+func _spawn_impact_marker(position: Vector3) -> void:
+	var marker = MeshInstance3D.new()
+	var box = BoxMesh.new()
+	box.size = Vector3(0.1, 0.1, 0.1)
+	marker.mesh = box
+
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color.RED
+	marker.set_surface_override_material(0, material)
+
+	get_tree().current_scene.add_child(marker)
+	marker.global_position = position
+
+	get_tree().create_timer(2.0).timeout.connect(marker.queue_free)
+
+
+func _spawn_projectile() -> void:
+	if not current_weapon.projectile_scene:
+		print("no projectile scnee assigned")
+		return
+
+	if not camera:
+		print("no camera assigned")
+		return
+
+	# spawn projectile and position at camera
+	var projectile = current_weapon.projectile_scene.instantiate() as Projectile
+	get_tree().current_scene.add_child(projectile)
+	projectile.global_position = camera.global_position
+
+	# calculate direction and velocity
+	var forward = -camera.global_transform.basis.z
+	var velocity = forward * current_weapon.projectile_speed
+	projectile.look_at(projectile.global_position + forward, Vector3.UP)
+
+	# setup projectile
+	projectile.setup(velocity, current_weapon.damage)
