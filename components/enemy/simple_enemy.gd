71fc39bc0b4f4e599df5f48615277687c8537ddb
Kevin Young <youngks93@gmail.com>
2026-05-24 16:54:55 -0400

    feat: basic enemy

---
+++ components/enemy/simple_enemy.gd
@@ -0,0 +1,70 @@
+class_name SimpleEnemy
+extends Enemy
+
+@export var follow_speed: float = 3.0
+@export var fall_gravity_multiplier = 1.5
+
+var target: Node3D
+
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var state_chart: StateChart = $StateChart
+@onready var health_component: HealthComponent = $HealthComponent
+
+
+func _ready():
+	super._ready()
+
+	target = get_tree().get_first_node_in_group("player")
+
+	health_component.died.connect(_on_died)
+	nav_agent.velocity_computed.connect(_on_velocity_computed)
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		# TODO: might want to expose the fall_gravity_multiplier as a cvar that every node can look up
+		velocity += get_gravity() * fall_gravity_multiplier * delta
+
+	move_and_slide()
+
+
+func on_triggered() -> void:
+	state_chart.send_event("toFollow")
+
+
+func _on_died() -> void:
+	queue_free()
+
+
+func _on_velocity_computed(safe_velocity: Vector3) -> void:
+	velocity.x = safe_velocity.x
+	velocity.z = safe_velocity.z
+
+
+func _on_follow_state_physics_processing(delta: float) -> void:
+	if not target:
+		return
+
+	nav_agent.target_position = target.global_position
+
+	# check if nav is finsihed
+	if nav_agent.is_navigation_finished():
+		nav_agent.velocity = Vector3.ZERO
+		return
+
+	# get next position in path
+	var next_pos = nav_agent.get_next_path_position()
+	var direction = (next_pos - global_position).normalized()
+
+	# set desired velo
+	nav_agent.velocity = direction * follow_speed
+
+	# rotate to face move direction
+	if direction.length() > 0.01:
+		var target_rotation = atan2(direction.x, direction.z)
+		rotation.y = lerp_angle(rotation.y, target_rotation, 5.0 * delta)
+
+
+func _on_detection_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		on_triggered()
